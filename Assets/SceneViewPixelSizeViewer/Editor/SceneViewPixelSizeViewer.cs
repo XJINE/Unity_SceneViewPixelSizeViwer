b112d827a79f5b253c61484280ca9645eea3ca02
XJINE <xjine.andorelseif@gmail.com>
2020-04-12 05:10:14 +0900

    First commit

---
+++ Assets/SceneViewPixelSizeViewer/Editor/SceneViewPixelSizeViewer.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[InitializeOnLoad]
+public class SceneViewPixelSizeViewer
+{
+    private static Vector2 PreviousSize;
+    private static int     FrameCount = 1;
+
+    static SceneViewPixelSizeViewer()
+    {
+        // NOTE:
+        // When use ver.2018 or before.
+        // SceneView.onSceneGUIDelegate += OnGUI;
+
+        SceneView.duringSceneGui += OnGUI;
+    }
+
+    private static void OnGUI(SceneView sceneView)
+    {
+        // CAUTION:
+        // A view ticked 1px size in same frame (bt. 1st and 2nd OnGUI).
+        // So needs to consider that case.
+
+        // NOTE:
+        // Rect rect = sceneView.position;
+        // position shows scaled size by OS and inclued SceneView's tool bar height.
+
+        GUILayout.BeginArea(new Rect(0, 0, 500, 100));
+
+        Vector2 size = new Vector2(sceneView.camera.pixelWidth,
+                                   sceneView.camera.pixelHeight);
+
+        if (PreviousSize != size)
+        {
+            FrameCount++;
+
+            if (FrameCount > 1)
+            {
+                GUILayout.Label("Size : " + size.x + " x " + size.y);
+            }
+        }
+        else
+        {
+            FrameCount = 0;
+        }
+
+        PreviousSize = size;
+
+        GUILayout.EndArea();
+    }
+}
+
+// NOTE:
+// Window type.
+
+//public class SceneViewInfo : EditorWindow
+//{
+//    [MenuItem("Window/SceneViewInfo")]
+//    static void Init()
+//    {
+//        GetWindow<SceneViewInfo>().Show();
+//    }
+
+//    protected virtual void OnEnable()
+//    {
+//        EditorApplication.update += Repaint;
+//    }
+
+//    protected virtual void OnDisable()
+//    {
+//        EditorApplication.update -= Repaint;
+//    }
+
+//    protected virtual void OnGUI()
+//    {
+//        foreach (var sceneView in Resources.FindObjectsOfTypeAll<SceneView>())
+//        {
+//            GUILayout.Label("Position.Size : " + sceneView.position.width
+//                                       + " x " + sceneView.position.height);
+
+//            GUILayout.Label("Camera.Pixel  : " + sceneView.camera.pixelWidth
+//                                       + " x " + sceneView.camera.pixelHeight);
+//        }
+//    }
+//}
--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # Unity_SceneViewPixelSizeViwer
+
+<img src="https://github.com/XJINE/Unity_SceneViewPixelSizeViwer/blob/master/Screenshot.png" width="100%" height="auto" />
+
+SceneViewPixelSizeViwer shows SceneView pixel size when the size changing.
+
+## Import to Your Project
+
+You can import this asset from UnityPackage.
+
+- [SceneViewPixelSizeViwer.unitypackage](https://github.com/XJINE/Unity_SceneViewPixelSizeViwer/blob/master/SceneViewPixelSizeViwer.unitypackage)
+
+## How to Use
+
+Just extract into your project.
